969db7c961d11ba820516a50cbd76f5c7f67ee58
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2025-03-16 15:03:01 +0000

    Fixed Map styling.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -26,9 +26,6 @@ export default function ContactPage() {
 			<h1>Contact Us</h1>
 			<div className="map">
 				<iframe
-					width="90%"
-					height="250"
-					style={{ border: 0 }}
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2232.2716520508266!2d-3.292032172948193!3d55.943045299878484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4887c707d04ceedd%3A0xbbb0601c0f2583d!2sAlmond%20River%20Records!5e0!3m2!1sen!2suk!4v1741922054158!5m2!1sen!2suk"
 					allowFullScreen
 					loading="lazy"
