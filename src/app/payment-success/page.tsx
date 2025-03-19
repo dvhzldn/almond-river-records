c55bcbf106b1a4564acf5b143db1ca392b71ea38
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2025-03-19 22:59:03 +0000

    Attempted another fix for deployment

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,15 +1,57 @@
-"use client";
+// app/payment-success/page.tsx
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 
-import dynamic from "next/dynamic";
+interface PaymentSuccessProps {
+	checkoutId: string | null;
+	title: string | null;
+	artist: string | null;
+	coverImage: string | null;
+}
 
-const PaymentSuccessClient = dynamic(() => import("./PaymentSuccessClient"), {
-	ssr: false,
-});
-
-export default function PaymentSuccess() {
+export default function PaymentSuccess({
+	checkoutId,
+	title,
+	artist,
+	coverImage,
+}: PaymentSuccessProps) {
 	return (
-		<div>
-			<PaymentSuccessClient />
+		<div className="page-container">
+			<h1 className="page-title">Payment Successful</h1>
+			<div className="content-box">
+				<h2>You have successfully ordered:</h2>
+				{artist && <h3>{artist}</h3>}
+				{title && <h3>{title}</h3>}
+				{coverImage && (
+					<Image
+						className="record-image"
+						src={coverImage}
+						alt={`Cover image for ${title}`}
+						width={500}
+						height={500}
+					/>
+				)}
+				<h2>Thank you for your purchase!</h2>
+
+				<br />
+				<h4>Your order will be processed and dispatched shortly.</h4>
+				<br />
+				{checkoutId && <p>Checkout ID: {checkoutId}</p>}
+			</div>
 		</div>
 	);
 }
+
+// Use getServerSideProps to handle the query parameters server-side
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const { query } = context;
+
+	return {
+		props: {
+			checkoutId: query.checkout_id || null,
+			title: query.title || null,
+			artist: query.artist || null,
+			coverImage: query.coverImage || null,
+		},
+	};
+};
