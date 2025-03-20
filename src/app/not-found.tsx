457e2139049f1756d19eb7bdc8c42f86069c9817
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2025-03-20 02:23:39 +0000

    404 page added.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+	return (
+		<div className="page-container">
+			<h1 className="page-title">Page not found</h1>
+			<div className="content-box">
+				<h2>
+					We are sorry, but the page you were looking for does not exist.
+				</h2>
+				<p>
+					If you are having any difficulty with the site, you can send us a
+					message from our{" "}
+					<Link href="/contact" className="hyperLink">
+						contact page
+					</Link>
+					.
+				</p>
+				<br />
+				<p>
+					Or you can return to the{" "}
+					<Link href="/" className="hyperLink">
+						homepage
+					</Link>
+					.
+				</p>
+			</div>
+		</div>
+	);
+}
