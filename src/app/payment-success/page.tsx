02ffd825cb76caa778753f3655bef8b25da2b684
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2025-03-21 18:58:18 +0000

    Payment success testing changes

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function PaymentSuccess({ searchParams }) {
 	const checkoutId = searchParams.checkout_id;
-	const title = searchParams.title;
-	const artist = searchParams.artist;
-	const coverImage = searchParams.coverImage;
+	// const title = searchParams.title;
+	// const artist = searchParams.artist;
+	// const coverImage = searchParams.coverImage;
 
 	return (
 		<div className="page-container">
 			<h1 className="page-title">Payment Successful</h1>
 			<div className="content-box">
-				<h2>You have successfully ordered:</h2>
+				{/* <h2>You have successfully ordered:</h2>
 				{artist && <h3>{artist}</h3>}
 				{title && <h3>{title}</h3>}
 				{coverImage && (
@@ -21,7 +21,7 @@ export default function PaymentSuccess({ searchParams }) {
 						width={500}
 						height={500}
 					/>
-				)}
+				)} */}
 				<h2>Thank you for your purchase!</h2>
 				<br />
 				<h4>Your order will be processed and dispatched shortly.</h4>
